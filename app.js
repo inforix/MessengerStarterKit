3964c124a4e3d08c13c323590c75eb2b55487d3d
wyp <wyp@shmtu.edu.cn>
2020-06-12 23:16:00 +0800

    add ipfilter & cache

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 //var path = require('path');
 var cookieParser = require('cookie-parser');
+const ipfilter = require('express-ipfilter').IpFilter
+
+
 //var logger = require('morgan');
 var logger = require('./services/log');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+// Whitelist the following IPs
+// const ips = ['127.0.0.1'];
+
+// IP Restriction
+// app.use(ipfilter(ips, { mode: 'allow' }));
 
 //app.use(logger('dev'));
 app.use(logger.express);
